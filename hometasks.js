081077671e0818b18a0c634659b04a77d4a4fe8b
Raman Davidovich <raman.davidovich@gmail.com>
2025-02-26 17:53:21 +0300

    feat: finished lesson 1 hometasks

---
+++ hometasks.js
@@ -0,0 +1,25 @@
+const tasks = [
+  [
+    {
+      description: `Try to confuse others with an unusual "console.log" using non-obvious JS tricks =)<br />
+    <br />
+    Important: the complexity of an expression should not lie in its length.
+    <br />
+    <br />
+    <img id="lesson1Task1DescriptionImage" class="image" />`,
+      githubLink:
+        "https://github.com/raman-davidovich/JS_traineeship/blob/main/Lesson1/solutions/lesson1Task1.js",
+    },
+    {
+      description: `Transform a number into a binary form.<br />
+      <br />
+      Optionally:<br />
+      *output the number by any method<br />
+      *receive a number from user by any input method.`,
+      githubLink:
+        "https://github.com/raman-davidovich/JS_traineeship/blob/main/Lesson1/solutions/lesson1Task2.js",
+    },
+  ],
+];
+
+export default tasks;
